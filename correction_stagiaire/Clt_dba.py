881b06afc9b7d489a68596eec0ac107109f68810
DBA <baek.david@gmail.com>
2022-09-12 16:55:53 +0200

    CorrectionTheoAdded

--- correction_stagiaire/Clt_dba.py
+++ correction_stagiaire/Clt_dba.py
@@ -1,10 +1,16 @@
 #Socket client
 import socket
+import time
 
 host, port_ecoute = ('192.168.1.31', 5555) #SrvVmUb_dba -.31
 
 mon_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mon_socket.connect((host, port_ecoute))
 print("connexion réussie sur :", host, ":", port_ecoute)
-address = mon_socket.accept()
-print("connexion entrante réussie", address)
+
+# address = mon_socket.accept()
+# print("connexion entrante réussie", address)
+
+# msg = "Je suis le client"
+# msg =msg.encode("utf8")
+mon_socket.send("Je suis DBA Client".encode())
