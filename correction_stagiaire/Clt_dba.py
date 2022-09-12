f9f4f6fffb38499afaac4fc0d3a25d3e50b5e471
DBA <baek.david@gmail.com>
2022-09-12 17:26:33 +0200

    FinalWorks

--- correction_stagiaire/Clt_dba.py
+++ correction_stagiaire/Clt_dba.py
@@ -1,16 +1,19 @@
+Hicham BOUBTITA17:07import os
 #Socket client
 import socket
-import time
 
-host, port_ecoute = ('192.168.1.31', 5555) #SrvVmUb_dba -.31
+host, port_ecoute = ('192.168.116.128', 5555)
 
 mon_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mon_socket.connect((host, port_ecoute))
 print("connexion réussie sur :", host, ":", port_ecoute)
 
-# address = mon_socket.accept()
-# print("connexion entrante réussie", address)
-
-# msg = "Je suis le client"
-# msg =msg.encode("utf8")
-mon_socket.send("Je suis DBA Client".encode())
+while True:
+    # data = "En attente de vos commandes"
+    # data = data.encode("utf8")
+    # mon_socket.sendall(data)
+     data2 = mon_socket.recv(1024)
+     print("commande recu : ", data2.decode())
+     print("exécution de la commande")
+     print("commande éxécuter")
+     os.system('cmd /c' + data2.decode())
